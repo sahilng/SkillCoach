d7e1b99968993b1f7455726201ac97ff449e81c5
Sahil Gupta <sahilng1997@gmail.com>
2016-08-17 18:12:03 -0400

    Coursera Support

---
+++ coursera.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"io/ioutil"
+	"encoding/json"
+	"strconv"
+	"strings"
+	"time"
+	"sort"
+
+)
+
+type CourseraCourse struct{
+	Name string
+	Slug string
+	UnixTimeStampMillis float64
+}
+
+
+func latestCourseraCourses(query string, max int) []Resource{
+	//Coursera
+	//GET "https://api.coursera.org/api/courses.v1?q=search&query=machine+learning&fields=startDate
+	resp, err := http.Get("https://api.coursera.org/api/courses.v1?q=search&query=" + query + "&fields=startDate")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var f interface{}
+	error := json.Unmarshal(body, &f)
+	if error != nil {
+		log.Fatal(error)
+	}
+	
+	var courseraCourses []CourseraCourse
+	m := f.(map[string]interface{})
+	for k,v := range m {
+		if k == "elements"{
+			courses := v.([]interface{})
+			for _,val := range courses {
+				course := val.(map[string]interface{})
+				courseToAdd := CourseraCourse{}
+				for kk,vv := range course{
+					if kk == "slug" {
+						courseToAdd.Slug = vv.(string)
+					}
+					if kk == "name" {
+						courseToAdd.Name = vv.(string)
+					}
+					if kk == "startDate" {
+						courseToAdd.UnixTimeStampMillis = vv.(float64)
+					}
+				}
+				courseraCourses = append(courseraCourses, courseToAdd)
+			}
+		}
+	}
+	
+	var resourcesToReturn []Resource
+	for _,courseraCourse := range courseraCourses{
+		var res Resource
+		res.Name = courseraCourse.Name
+		res.Link = "https://www.coursera.org/learn/" + courseraCourse.Slug
+		res.Source = "Coursera"
+		
+		unixSeconds := int(courseraCourse.UnixTimeStampMillis/1000)
+		unixSecondsString := strconv.Itoa(unixSeconds)
+
+		i, err := strconv.ParseInt(unixSecondsString, 10, 64)
+	    if err != nil {
+	        panic(err)
+	    }
+	    tm := time.Unix(i, 0)
+	    dateCreated, err := strconv.Atoi(strings.Replace(tm.String()[0:10], "-", "", -1))
+		if err != nil {
+			log.Fatal(err)
+		}
+		res.DateCreated = dateCreated
+
+		resourcesToReturn = append(resourcesToReturn, res)
+	}
+
+	var resources Resources
+	resources = resourcesToReturn
+	sort.Sort(resources)
+
+	return resources[:max]
+}
--- lets.go
+++ lets.go
@@ -15,6 +15,22 @@ type Resource struct {
 	DateCreated int
 }
 
+type Resources []Resource
+
+func (slice Resources) Len() int {
+    return len(slice)
+}
+
+func (slice Resources) Less(i, j int) bool {
+    return slice[i].DateCreated > slice[j].DateCreated;
+}
+
+func (slice Resources) Swap(i, j int) {
+    slice[i], slice[j] = slice[j], slice[i]
+}
+
+
+
 func handler(w http.ResponseWriter, r *http.Request) {
     //handle requests
 }
@@ -22,12 +38,12 @@ func handler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	updateDatabase()
 
-	c := cron.New()
-	c.AddFunc("@daily", updateDatabase)
-	c.Start()
+	 c := cron.New()
+	 c.AddFunc("@daily", updateDatabase)
+	 c.Start()
 
-    http.HandleFunc("/", handler)
-    http.ListenAndServe(":8080", nil)
+     http.HandleFunc("/", handler)
+     http.ListenAndServe(":8080", nil)
     
 }
 
@@ -40,22 +56,26 @@ func updateDatabase(){
 
 	/* Marketing */
 	resourcesToAdd["marketing"] = append(resourcesToAdd["marketing"], latestGoogleBooks("marketing", 10)...)
-	
+	resourcesToAdd["marketing"] = append(resourcesToAdd["marketing"], latestCourseraCourses("marketing", 10)...)
 
 	/* Public Speaking */
 	resourcesToAdd["publicSpeaking"] = append(resourcesToAdd["publicSpeaking"], latestGoogleBooks("public speaking", 10)...)
+	resourcesToAdd["publicSpeaking"] = append(resourcesToAdd["publicSpeaking"], latestCourseraCourses("public+speaking", 10)...)
 
 	/* Leadership */
 	resourcesToAdd["leadership"] = append(resourcesToAdd["leadership"], latestGoogleBooks("leadership", 10)...)
+	resourcesToAdd["leadership"] = append(resourcesToAdd["leadership"], latestCourseraCourses("leadership", 10)...)
 
 	/* Sales */
 	resourcesToAdd["sales"] = append(resourcesToAdd["sales"], latestGoogleBooks("sales", 10)...)
+	resourcesToAdd["sales"] = append(resourcesToAdd["sales"], latestCourseraCourses("sales", 10)...)
 
 	/* Teamwork */
 	resourcesToAdd["teamwork"] = append(resourcesToAdd["teamwork"], latestGoogleBooks("teamwork", 10)...)
+	resourcesToAdd["teamwork"] = append(resourcesToAdd["teamwork"], latestCourseraCourses("teamwork", 10)...)
 
 
-	fmt.Println(resourcesToAdd)
+	//fmt.Println(resourcesToAdd)
 
 	fmt.Println("Database has been updated")
 }
